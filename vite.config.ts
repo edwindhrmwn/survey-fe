9c593a9cbf9a8804b8079bf93c9d7a9b0ca019f2
Edwin Dharmawan <edwindharmawan@gmail.com>
2024-07-05 08:24:56 +0700

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    preserveSymlinks: true,
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
